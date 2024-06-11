29a40eb10a5af3d06c5ecf53f64f7571b5c01e59
unknown <anushkaghalayan@gmail.com>
2024-06-11 09:46:12 +0400

    Js changes

--- js/indexScript.js
+++ js/indexScript.js
@@ -26,8 +26,27 @@ const images = [
   
   setInterval(changeBackgroundImage, 3000);
   
-//   libraryElements.forEach((img, index) => {
-//     img.addEventListener('click', () => {
-//       backImageElement.style.backgroundImage = images[index];
-//     });
-//   });
+  libraryElements.forEach((img, index) => {
+    img.addEventListener('click', () => {
+      backImageElement.style.backgroundImage = images[index];
+    });
+  });
+
+
+ function checkInput() { 
+var query = document.getElementById('search').value; //vercnel search- i bovandakutyuny 
+var content = document.getElementById('content'); // vercnel te vortexic petq e gtnven mutqagrvac simvolnery kam barery       
+ removeHighlights(content);
+ if (query) { 
+highlightText(content, query); 
+} 
+ } 
+function highlightText(content, query) { 
+  var regex = new RegExp('(' + query + ')', 'gi'); // regex artahaytutyun  search - i mej grvacy gtnelu hamar, 
+// kap chuni mecatar te poqratar 
+content.innerHTML = content.innerHTML.replace(regex, '<span class="highlight">$1</span>'); // yndgcel hamynknumnery 
+} 
+  
+function removeHighlights(content) { 
+content.innerHTML = content.innerHTML.replace(/<span class="highlight">(.*?)<\/span>/g, '$1'); // jnjel naxkin hamynknumnery 
+} 
