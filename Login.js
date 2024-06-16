a365e7fe5d3937aef4607f9f65d77e2891855b5b
JuliaSimonyan <julietasimonyan2001@mail.ru>
2024-06-16 11:22:34 +0400

    Translation done, footer links

--- Login.js
+++ Login.js
@@ -1,3 +1,12 @@
-function googleTranslateElementInit() {
-    new google.translate.TranslateElement({ pageLanguage: 'am' }, 'google_translate_element');
-}
+
+    function googleTranslateElementInit() {
+        new google.translate.TranslateElement({
+            pageLanguage: 'en',
+            includedLanguages: 'en,ru,hy',
+            autoDisplay: false,
+            layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+        }, 'google_translate_element');
+    }
+
+
+
